ffdd052073306257fa3df89a64a195357a42ef5c
vasilisasmirnova <vasilisa.a.smirnova@gmail.com>
2020-05-26 00:04:33 +0300

    Третья защита, дорабатывает БЭМ-разметку

--- source/js/menu-script.js
+++ source/js/menu-script.js
@@ -2,20 +2,20 @@ var navMain = document.querySelector('.main-nav__wrapper');
 var navOpen = document.querySelector('.main-nav__open');
 var navClose = document.querySelector('.main-nav__close');
 
-navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove('main-nav__wrapper--nojs');
 
 navOpen.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-    navOpen.classList.add('main-nav__open--hide');
+  if (navMain.classList.contains('main-nav__wrapper--closed')) {
+    navMain.classList.remove('main-nav__wrapper--closed');
+    navMain.classList.add('main-nav__wrapper--opened');
+    navOpen.classList.add('main-nav__hide');
   }
 });
 
 navClose.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--opened')) {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-    navOpen.classList.remove('main-nav__open--hide');
+  if (navMain.classList.contains('main-nav__wrapper--opened')) {
+    navMain.classList.add('main-nav__wrapper--closed');
+    navMain.classList.remove('main-nav__wrapper--opened');
+    navOpen.classList.remove('main-nav__hide');
   }
 });
